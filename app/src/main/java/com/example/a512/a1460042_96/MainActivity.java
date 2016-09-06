87b05a99f093be608ac5927dff998c50998fe0e6
wonyongseon <wonyongseon@host.com>
2016-09-06 11:16:52 +0900

    원용선

---
+++ app/src/main/java/com/example/a512/a1460042_96/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.a512.a1460042_96;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button btName;
+    public TextView txName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txName=(TextView)findViewById(R.id.txName);
+        btName=(Button)findViewById(R.id.btName);
+
+        btName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txName.setText("Won");
+                Toast.makeText(getApplicationContext(),"Won",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
